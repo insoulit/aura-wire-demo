2df6dba50015067880fd5bc68a4f91931e69a7ed
alpondith <sujonalimran@gmail.com>
2026-08-20 18:27:41 +0200

    refactor: reorganize user settings layout into two distinct grid rows and update spacing

--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -62,13 +62,14 @@ class extends Component {};
     </x-aura::card>
 
     <!-- 2. Account & Security Module Cards Grid -->
-    <x-aura::flex direction="col" gap="3.5">
+    <x-aura::flex direction="col" gap="4">
 
         <x-aura::heading level="2" size="sm">
             Account and Security
         </x-aura::heading>
 
-        <x-aura::grid cols="1" sm="2" lg="3" gap="4">
+        <!-- Row 1: Profile, Avatar, Email -->
+        <x-aura::grid cols="1" sm="3" gap="4">
 
             <!-- 1. Profile Info -->
             <x-aura::card href="/user/profile" wire:navigate gap="4">
@@ -181,6 +182,11 @@ class extends Component {};
 
             </x-aura::card>
 
+        </x-aura::grid>
+
+        <!-- Row 2: Security & Settings -->
+        <x-aura::grid cols="1" sm="2" gap="4">
+
             <!-- 4. Change Password & Security -->
             <x-aura::card href="/user/password" wire:navigate gap="4">
 
@@ -219,45 +225,41 @@ class extends Component {};
             </x-aura::card>
 
             <!-- 5. Workspace Settings -->
-            <div class="sm:col-span-2 lg:col-span-2 h-full">
+            <x-aura::card href="/user/setting" wire:navigate gap="4">
 
-                <x-aura::card href="/user/setting" wire:navigate gap="4">
+                <x-aura::flex align="start" justify="between" gap="3">
 
-                    <x-aura::flex align="start" justify="between" gap="3">
+                    <x-aura::icon name="sliders" size="lg" :container="true" />
 
-                        <x-aura::icon name="sliders" size="lg" :container="true" />
+                    <x-aura::badge variant="subtle" size="sm">
+                        Preferences
+                    </x-aura::badge>
 
-                        <x-aura::badge variant="subtle" size="sm">
-                            Preferences
-                        </x-aura::badge>
+                </x-aura::flex>
 
-                    </x-aura::flex>
+                <x-aura::flex direction="col" gap="1">
 
-                    <x-aura::flex direction="col" gap="1">
+                    <x-aura::heading level="3" size="sm">
+                        Account and Workspace Settings
+                    </x-aura::heading>
 
-                        <x-aura::heading level="3" size="sm">
-                            Account and Workspace Settings
-                        </x-aura::heading>
+                    <x-aura::text variant="subtle" size="sm">
+                        Configure system preferences, notification alerts, and theme preferences.
+                    </x-aura::text>
 
-                        <x-aura::text variant="subtle" size="sm">
-                            Configure system preferences, notification alerts, and theme preferences.
-                        </x-aura::text>
+                </x-aura::flex>
 
-                    </x-aura::flex>
+                <x-slot:footer>
 
-                    <x-slot:footer>
+                    <x-aura::text size="sm" weight="semibold">
+                        Configure
+                    </x-aura::text>
 
-                        <x-aura::text size="sm" weight="semibold">
-                            Configure
-                        </x-aura::text>
+                    <x-aura::icon name="arrow-right" size="xs" />
 
-                        <x-aura::icon name="arrow-right" size="xs" />
+                </x-slot:footer>
 
-                    </x-slot:footer>
-
-                </x-aura::card>
-
-            </div>
+            </x-aura::card>
 
         </x-aura::grid>
 
